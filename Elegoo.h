b7bc00a547c0e07c29cb5504b8aa35a6e15ae436
danprof <60632352+danprof@users.noreply.github.com>
2020-11-22 17:35:11 -0500

    Add files via upload

--- Elegoo.h
+++ Elegoo.h
@@ -9,24 +9,24 @@ extern Servo myservo;
 class Elegoo 
 {
 
-  #define ENA 5
-  #define ENB 6
-  #define IN1 7
+  #define ENA 5		// left and right motor Enables
+  #define ENB 6		
+  #define IN1 7		// motor direction control pins
   #define IN2 8
   #define IN3 9
   #define IN4 11
-  #define LED 13
-  #define RECV_PIN 12
-  #define Echo  A4  
-  #define Trig  A5
+  #define LED 13	// built in LED
+  #define RECV_PIN 12	// IR receiver
+  #define Echo  A4  	// sonar Echo
+  #define Trig  A5	// sonar Trig
 
 private:
-   bool state = LOW;
+   bool state = LOW;		// current drive state (forward,reverse,stop)
    int fast_speed = 150;
    int med_speed = 100;
    int slow_speed = 75;
    int motorSpeed = med_speed;
-   int rightDistance = 0;
+   int rightDistance = 0;	// sonar readings
    int leftDistance = 0;
    int middleDistance = 0;
 
@@ -42,7 +42,7 @@ public:
    void Left(int);
    void Right(int);
    void stopCar();
-   void stateChange();
+   void stateChange();		// toggle light
    char execute(char);
    void irCommand(int code);
    int Distance_test();
